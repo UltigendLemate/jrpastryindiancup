288ff4c3c6652f7d942322b2ccc42fee4677106b
muXtract <56767342+muXtract@users.noreply.github.com>
2023-06-05 17:18:42 +0530

    bg

--- components/Organizers/index.tsx
+++ components/Organizers/index.tsx
@@ -9,35 +9,40 @@ import styles from "../../styles/Organizers.module.css"
 const index = () => {
   return (
     
-    <div className="bg-[#FB8DA0]/60 " >
+    <div className={`${styles.customBackground} bg-[length:40%] bg-center `} >
         <div className=' text-[#EFEBE0]  text-4xl md:text-5xl p-3 mx-auto  font-semibold   flex justify-center'>Our Organizers</div>
-      <div className="sm:grid sm:grid-cols-2 max-w-6xl m-auto gap-4 md:gap-6 p-5 ">
+      <div className="sm:grid sm:grid-cols-2 max-w-5xl m-auto gap-4 md:gap-8 p-4  ">
        
         {/* CARD1 */}
-   <div className="max-w-xl mb-5 sm:mb-0 mx-auto   bg-white rounded-md  overflow-hidden shadow-md">
-        <Image src={image1} alt="Image1" className='w-full p-5' />
+   <div className="max-w-xs md:max-w-lg  mb-5 sm:mb-0 mx-auto p-2 sm:p-5  bg-white rounded-md  overflow-hidden shadow-md">
+    <div className="flex w-full  h-32 sm:h-36 justify-center">
+        <Image  src={image1} alt="Image1" className='w-80 p-4' />
+    </div>
         <div className="p-4">
         <p className="text-gray-800 text-xs md:text-sm mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ad nesciunt consequuntur error modi explicabo ipsum voluptas sit incidunt natus soluta repudiandae eos minima corporis, quisquam, a totam numquam alias. Labore animi, explicabo laudantium impedit dolore a eveniet maiores nobis quae numquam assumenda sunt eos et, earum reprehenderit voluptatem ducimus.</p>
         </div>
     </div>
     {/* CARD2 */}
-   <div className="max-w-xl  mb-5 sm:mb-0 mx-auto   bg-white rounded-md overflow-hidden shadow-md">
-        <Image src={image2} alt="Image1" className='w-full p-5' />
-        <div className="p-4">
+   <div className="max-w-xs md:max-w-lg   mb-5 sm:mb-0 mx-auto p-2 sm:p-5  bg-white rounded-md overflow-hidden shadow-md">
+   <div className="flex w-full  h-32 sm:h-36 justify-center">
+        <Image  src={image2} alt="Image1" className='w-80 p-4' />
+    </div> <div className="p-4">
         <p className="text-gray-800 text-xs md:text-sm mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ad nesciunt consequuntur error modi explicabo ipsum voluptas sit incidunt natus soluta repudiandae eos minima corporis, quisquam, a totam numquam alias. Labore animi, explicabo laudantium impedit dolore a eveniet maiores nobis quae numquam assumenda sunt eos et, earum reprehenderit voluptatem ducimus.</p>
           </div>
     </div>
     {/* CARD3 */}
-   <div className="max-w-xl mb-5 sm:mb-0  mx-auto  bg-white rounded-md overflow-hidden shadow-md">
-        <Image src={image3} alt="Image1" className='w-full p-5' />
-        <div className="p-4">
+   <div className="max-w-xs md:max-w-lg  mb-5 sm:mb-0  mx-auto p-2 sm:p-5 bg-white rounded-md overflow-hidden shadow-md">
+   <div className="flex w-full  h-32 sm:h-36 justify-center">
+        <Image  src={image3} alt="Image1" className='w-80 p-4' />
+    </div> <div className="p-4">
         <p className="text-gray-800 text-xs md:text-sm mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ad nesciunt consequuntur error modi explicabo ipsum voluptas sit incidunt natus soluta repudiandae eos minima corporis, quisquam, a totam numquam alias. Labore animi, explicabo laudantium impedit dolore a eveniet maiores nobis quae numquam assumenda sunt eos et, earum reprehenderit voluptatem ducimus.</p>
          </div>
     </div>
     {/* CARD4 */}
-   <div className="max-w-xl mb-5 sm:mb-0 mx-auto   bg-white rounded-md overflow-hidden shadow-md">
-        <Image src={image4} alt="Image1" className='w-full p-5' />
-        <div className="p-4">
+   <div className="max-w-xs md:max-w-lg  mb-5 sm:mb-0 mx-auto p-2 sm:p-5  bg-white rounded-md overflow-hidden shadow-md">
+   <div className="flex w-full  h-32 sm:h-36 justify-center">
+        <Image  src={image4} alt="Image1" className='w-80 p-4' />
+    </div> <div className="p-4">
         <p className="text-gray-800 text-xs md:text-sm mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ad nesciunt consequuntur error modi explicabo ipsum voluptas sit incidunt natus soluta repudiandae eos minima corporis, quisquam, a totam numquam alias. Labore animi, explicabo laudantium impedit dolore a eveniet maiores nobis quae numquam assumenda sunt eos et, earum reprehenderit voluptatem ducimus.</p>
         </div>
     </div>
--- components/Participants/index.tsx
+++ components/Participants/index.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import data from "../Participants/participants.json"
-import { useCollapse } from 'react-collapsed'
+import image1 from "../images/chef.jpg"
 
+import { FaTimes } from 'react-icons/fa';
 
-const index = () => {
-    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+const index = () => {  const [showPopup, setShowPopup] = useState(false);
+  
+  const openPopup = () => {
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
     
   return (
     
     
-<div className="py-16 bg-gradient-to-br from-green-50 to-cyan-100">  
-    <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+<div className="py-10  bg-white">  
+    <div className="container m-auto  text-gray-600 md:px-12 xl:px-6">
         <div className="mb-12 space-y-2 text-center">
-            <h2 className="text-2xl text-cyan-900 font-bold md:text-4xl">Participants</h2>
+            <h2 className="text-2xl text-[#FF0080]   font-bold md:text-4xl">Participants</h2>
             
         </div>     
-        <div  className="grid gap-12 lg:grid-cols-4">
+        <div  className="grid gap-12  xl:grid-cols-4">
 
        
       {
@@ -25,29 +34,58 @@ const index = () => {
           {
 
           item.id % 4 !== 0 ? (
-           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                <Image width={200} height={50} className='w-[100%]'  src={item.imagePath} alt=" "/>
-                <div className="p-4">
-                    <h3 className="text-lg font-semibold mb-2">{item.name}</h3>
-                    <p className="text-gray-700">{item.text}</p>
+          //   <div className="card relative w-80 h-80 text-center m-auto bg-blue-200 max-w-sm">
+          //     <div className="discount  absolute bottom-0 left-[85px]">
+          //       <h5 className="bg-pink-500 inline absolute left-4 top-5  text-white font-bold">{item.name}</h5>
+          //     </div>
+          //     <div className="m-auto">
+          //     <Image height={200} width={200} src={image1} className=" card-img-top pt-5 w-full h-full" alt="..." />
+          //     </div>
+              
+          // </div>
+          <div className="card relative max-w-md flex justify-center  mx-auto xl:w-80 xl:h-80 h-72 w-72">
+            <Image alt='img' src={image1}  className='w-full p-3 h-full'/>
+            <p className='absolute bottom-5 left-5  p-1 bg-pink-700 rounded-md  text-white opacity-80 '>{item.name}</p>
 
-                </div>
-            </div>
+          </div>
+          
+
+          
           ) : (
-             <div>
-                        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                            <div className="p-4">
-                            <p className="text-base xl:text-sm 2xl:text-[19px]  mt-2">{item.normalText}</p>
-                        <div className=" text-base hidden lg:block">
-                            <section className='xl:text-sm 2xl:text-[19px]' {...getCollapseProps()}> {item.expandedText}</section>
-                            <button className="text-lg hidden xl:block text-gray-500 animate-pulse tracking-widest"
-                            {...getToggleProps()}>
-                                {isExpanded ? 'Collapse' : 'Expand'}
-                            </button>
-                        </div>
-                            </div>
-                         </div>
+          
+            <div className="flex flex-col items-center  bg-white rounded-lg  ">
+             <div className=''>
+            <div className="bg-white rounded-lg text-center xl:text-left max-w-xs m-auto  overflow-hidden">
+              <div className="p-4">
+                <p className="text-2xl md:text-3xl font-semibold  text-gray-700">{item.institute}</p>
+                <p className=" mt-3 text-xl text-gray-500">{item.city}</p>
+                <p className="mt-3  text-gray-500">{item.normalText}</p>
+                
+                <button
+                  className="text-[#FF0080]  hidden xl:block hover:underline mt-2"
+                  onClick={openPopup}
+                >
+                  Read More
+                </button>
+              </div>
             </div>
+           {showPopup && (
+              <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-20">
+                <div className="bg-white rounded-lg shadow-lg p-4 md:p-6 max-w-md w-full">
+                  <div className="flex justify-end">
+                    <button className="text-gray-600 hover:text-gray-800" onClick={closePopup}>
+                      <FaTimes className='text-pink-500 hover:text-pink-300' />
+                    </button>
+                  </div>
+                  <p className="text-base xl:text-sm 2xl:text-[19px]">
+                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit facere placeat sequi laborum asperiores itaque explicabo, a nihil, et molestias labore dolor corrupti? Consequuntur aliquid cumque, saepe repellat odit dicta corporis ab necessitatibus itaque autem iusto, placeat corrupti sed magnam, quasi non! Reprehenderit perferendis vel voluptatum consequatur qui omnis libero.
+                  </p>
+                  
+                </div>
+              </div>
+            )}
+          </div>
+           </div>
           
           )}
        </div>
