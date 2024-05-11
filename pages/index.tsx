4f5825f2ba0e86940787fdcfe0256ce6d76182cd
Chaitanya Anand <chaitanya.anand@gmail.com>
2024-05-12 02:56:17 +0530

    pastry queen india event linking added

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Sponsors from '@/components/Sponsors/Sponsors'
 import Organizers from '@/components/Organizers/index'
 import Participants from '@/components/Participants'
 import Jury from '@/components/Jury'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,13 @@ export default function Home() {
       <Participants />
       {/* <div className='fixed w-10 h-10 top-0 left-0 bg-black'> </div> */}
       <Contact />
+<div className='mx-auto mb-10 -mt-10'>
+        <h2 className='text-center text-2xl pb-5'>Same Dedication, New Event</h2>
+
+        <Link href="https://pastryqueenindia.com/">
+        <img src="banner.jpg" alt="pstry queen event logo" className='max-h-72 aspect-auto' />
+        </Link>
+      </div>
     </Layout>
   )
 }
