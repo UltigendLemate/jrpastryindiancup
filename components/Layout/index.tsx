3c11dc8e33d9dd24ac5711d5bd7866dbced7cb4c
Chaitanya Anand <chaitanya.anand@gmail.com>
2025-04-24 00:03:04 +0530

    nitpicks

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
 import Sponsors from "../Sponsors";
